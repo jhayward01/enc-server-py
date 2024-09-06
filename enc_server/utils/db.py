ba3aec9e6d65aa0d67028b94c5499da3d6add4ef
jhayward01 <johnny@Johns-MacBook-Pro.local>
2024-09-06 16:37:28 -0700

    Finishing db util and unit tests.

--- enc_server/utils/db.py
+++ enc_server/utils/db.py
@@ -1,30 +1,51 @@
 import abc
+import config
 import pymongo
-
-
-def get_record_collection():
-    print("Connecting to data store...")
-
-    my_client = pymongo.MongoClient("mongodb://localhost:27017/")
-    print(my_client.list_database_names())
 
 
 class DBInterface(abc.ABC):
     @abc.abstractmethod
-    def store_record(self, id: str, record: str):
+    def store_record(self, record_id: str, record_payload: str):
         pass
 
     @abc.abstractmethod
-    def retrieve_record(self, id: str) -> str:
+    def retrieve_record(self, record_id: str) -> str:
         pass
 
     @abc.abstractmethod
-    def delete_record(self, id: str):
+    def delete_record(self, record_id: str):
         pass
 
 
 class DB(DBInterface):
+    db_name = "enc-server-go"
+    db_collection = "records"
 
-    def __init__(self, server_addr):
-        self.server_host = server_addr.split(":")[0]
-        self.server_port = int(server_addr.split(":")[1])
+    def __init__(self, configs):
+        result, missing = config.verify_configs(configs, ["mongoURI"])
+        if not result:
+            raise KeyError("ConnSocket missing configuration" + missing)
+
+        self.mongo_uri = configs["mongoURI"]
+
+    def store_record(self, record_id: str, record_payload: str):
+        collection = self.__get_record_collection()
+        id_filter = {"_id": record_id}
+        entry = {"$set": {"_id": record_id, "record": record_payload}}
+        collection.update_one(id_filter, entry, upsert=True)
+
+    def retrieve_record(self, record_id: str) -> str:
+        collection = self.__get_record_collection()
+        entry = collection.find_one({"_id": record_id})
+        record_payload = entry["record"]
+        return record_payload
+
+    def delete_record(self, record_id: str):
+        collection = self.__get_record_collection()
+        collection.delete_one({"_id": record_id})
+
+    def __get_record_collection(self):
+        client = pymongo.MongoClient(self.mongo_uri)
+        db = client[DB.db_name]
+        collection = db[DB.db_collection]
+        return collection
--- test/test_utils_db.py
+++ test/test_utils_db.py
@@ -3,7 +3,26 @@ import unittest
 
 
 class ConnTestSuite(unittest.TestCase):
-    enc_server.utils.get_record_collection()
+    record_id = "JTH"
+    record_payload = "PAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADS"
+
+    mongo_uri = "mongodb://localhost:27017/"
+    db = enc_server.utils.DB({"mongoURI": mongo_uri})
+
+    def test_init(self):
+        self.assertIsNotNone(ConnTestSuite.db)
+
+    def test_store_record(self):
+        self.assertIsNone(ConnTestSuite.db.store_record(ConnTestSuite.record_id, ConnTestSuite.record_payload))
+
+    def test_retrieve_record(self):
+        self.test_store_record()
+        record_payload = ConnTestSuite.db.retrieve_record(ConnTestSuite.record_id)
+        self.assertEqual(ConnTestSuite.record_payload, record_payload)
+
+    def test_delete_record(self):
+        self.test_store_record()
+        self.assertIsNone(ConnTestSuite.db.delete_record(ConnTestSuite.record_id))
 
 
 if __name__ == '__main__':
