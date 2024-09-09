f78f4c2b61827fe441ebf18ef7ecd5bf7686e365
jhayward01 <johnny@Johns-MacBook-Pro.local>
2024-09-09 14:58:59 -0700

    Updating unit tests.

--- test/test_utils_db.py
+++ test/test_utils_db.py
@@ -7,22 +7,17 @@ class ConnTestSuite(unittest.TestCase):
     record_payload = "PAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADSPAYLOADS"
 
     mongo_uri = "mongodb://localhost:27017/"
-    db = enc_server.utils.DB({"mongoURI": mongo_uri})
+
+    def setUp(self):
+        self.db = enc_server.utils.DB({"mongoURI": ConnTestSuite.mongo_uri})
 
     def test_init(self):
-        self.assertIsNotNone(ConnTestSuite.db)
+        self.assertIsNotNone(self.db)
 
-    def test_store_record(self):
-        self.assertIsNone(ConnTestSuite.db.store_record(ConnTestSuite.record_id, ConnTestSuite.record_payload))
-
-    def test_retrieve_record(self):
-        self.test_store_record()
-        record_payload = ConnTestSuite.db.retrieve_record(ConnTestSuite.record_id)
-        self.assertEqual(ConnTestSuite.record_payload, record_payload)
-
-    def test_delete_record(self):
-        self.test_store_record()
-        self.assertIsNone(ConnTestSuite.db.delete_record(ConnTestSuite.record_id))
+    def test_db(self):
+        self.assertIsNone(self.db.store_record(ConnTestSuite.record_id, ConnTestSuite.record_payload))
+        self.assertEqual(ConnTestSuite.record_payload, self.db.retrieve_record(ConnTestSuite.record_id))
+        self.assertIsNone(self.db.delete_record(ConnTestSuite.record_id))
 
 
 if __name__ == '__main__':
