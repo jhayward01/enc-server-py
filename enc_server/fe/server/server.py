d592a940fbd173758a54c8d2b0b940cc76381080
jhayward01 <johnny@Johns-MacBook-Pro.local>
2024-09-13 09:53:58 -0700

    Adding FE client code.

--- enc_server/fe/server/server.py
+++ enc_server/fe/server/server.py
@@ -7,6 +7,7 @@ class Server(utils.Responder):
     def __init__(self, configs: dict):
         self.keygen = utils.Keygen(configs)
         self.be_client = be.client.Client(configs)
+        self.socket_io = utils.SocketIO(configs, self)
 
     def respond(self, msg: bytes) -> bytes:
         s = msg.decode('utf-8').strip()
@@ -19,42 +20,31 @@ class Server(utils.Responder):
         if fields[0] == "STORE":
             try:
                 key, nonce = self.keygen.random_key(), self.keygen.random_nonce()
-                cipher = utils.Cipher({"key": key, "nonce": nonce})
-                id_enc = cipher.encrypt(fields[1].encode('utf-8')).decode('utf-8')
-                record_enc = cipher.encrypt(fields[2].encode('utf-8')).decode('utf-8')
+                cipher = utils.Cipher(key, nonce)
+                id_enc = cipher.encrypt(fields[1])
+                record_enc = cipher.encrypt(fields[2])
                 self.be_client.store(id_enc, record_enc)
             except RuntimeError as err:
                 return f"ERROR: {err}\n".encode('utf-8')
             else:
                 return key
-
-            # func (s *serverImpl) storeRecord(id, record []byte) (key []byte, err error) {
-            #
-            # 	// Generate cipher entry for ID.
-            # 	idEncrypt := s.idCipher.Seal(s.idNonce, s.idNonce, id, nil)
-            #
-            # 	// Generate random AES key.
-            # 	if key, err = s.keygen.RandomKey(); err != nil {
-            # 		return nil, err
-            # 	}
-            #
-            # 	// Generate key, cipher, and nonce for record.
-            # 	cipher, err := s.keygen.GetGCMCipher(key)
-            # 	if err != nil {
-            # 		return nil, err
-            # 	}
-            #
-            # 	// Randomly generate nonce (initialization vector).
-            # 	nonce, err := s.keygen.RandomNonce(cipher.NonceSize())
-            # 	if err != nil {
-            # 		return nil, err
-            # 	}
-            #
-            # 	// Generate cipher entry for record. Place in data store.
-            # 	recordEncrypt := cipher.Seal(nonce, nonce, record, nil)
-            # 	if err = s.beClient.StoreRecord(idEncrypt, recordEncrypt); err != nil {
-            # 		return nil, err
-            # 	}
-            #
-            # 	return key, nil
-            # }
+        elif fields[0] == "RETRIEVE":
+            try:
+                key = fields[2].encode('utf-8')
+                cipher = utils.Cipher(key, bytes())
+                id = cipher.decrypt(fields[1])
+                payload = self.be_client.retrieve(id)
+            except RuntimeError as err:
+                return f"ERROR: {str(err)}\n".encode('utf-8')
+            else:
+                return (payload + "\n").encode('utf-8')
+        elif fields[10] == "DELETE":
+            try:
+                key = fields[2].encode('utf-8')
+                cipher = utils.Cipher(key, bytes())
+                id = cipher.decrypt(fields[1])
+                payload = self.be_client.delete(id)
+            except RuntimeError as err:
+                return f"ERROR: {str(err)}\n".encode('utf-8')
+            else:
+                return "\n".encode('utf-8')
